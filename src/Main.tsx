5897b01bac0fa618857951cbbcd52a01d134b1e2
Paulo Teran <paulo.teran@gmail.com>
2022-06-13 21:09:29 -0400

    Added router

---
+++ src/Main.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+
+const Main = () => {
+return (         
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+
+  </Routes>
+);
+}
+export default Main;
---
+++ src/Pages/About.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+export const About = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is the about page
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
